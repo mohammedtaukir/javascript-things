d2ffd33216581ec3851b7a1542ba7ebebe849afe
mohammedtaukir <122686429+mohammedtaukir@users.noreply.github.com>
2023-03-16 20:16:26 +0545

    Create browser function.js

---
+++ browser function.js
@@ -0,0 +1,18 @@
+some common browser functions in JavaScript:
+
+    document.getElementById() - Finds an HTML element by its id attribute.
+    document.querySelector() - Finds the first matching element for a CSS selector.
+    document.createElement() - Creates a new HTML element.
+    element.innerHTML - Gets or sets the HTML content inside an element.
+    element.style - Gets or sets the CSS styles for an element.
+    window.location - Gets or sets the current URL of the page.
+    window.alert() - Displays an alert box with a message.
+    window.prompt() - Displays a dialog box for the user to enter input.
+    window.confirm() - Displays a dialog box with a message and OK and Cancel buttons.
+    window.open() - Opens a new browser window or tab.
+    window.close() - Closes the current browser window.
+    window.setTimeout() - Sets a delay before running a function.
+    window.setInterval() - Runs a function at a specified interval.
+    XMLHttpRequest - Creates an object for making HTTP requests.
+    fetch() - A modern alternative to XMLHttpRequest for making HTTP requests.
+
